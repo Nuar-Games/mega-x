d9d0391235c2d9a69c20554888c4cb5807527f5b
Nuar-Games <nuargita96@gmail.com>
2026-08-31 14:02:30 +0800

    revert: remove broken real-phone Arena override from production

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './audio.ts'
 import './lobby-ui.ts'
-import './arena-real-phone.css'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
