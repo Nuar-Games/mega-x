485356a796d5f3d5c86cb899ff9d86403b9ca427
Nuar-Games <nuargita96@gmail.com>
2026-09-01 23:07:18 +0800

    Load final lobby containment styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './lobby-ui.ts'
 import App from './App.tsx'
 import './arena-stage.css'
 import './arena-stage.ts'
+import './lobby-final.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />,
