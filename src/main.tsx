ca7b8195cc9c3124008d64fb273b8545a0601ab1
Nuar-Games <nuargita96@gmail.com>
2026-09-02 09:32:23 +0800

    Load MEGA-X main landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './audio.ts'
+import './landing-ui.ts'
 import './lobby-ui.ts'
 import App from './App.tsx'
 import './arena-stage.css'
