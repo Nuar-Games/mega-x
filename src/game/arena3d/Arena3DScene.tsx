b79176edf32f65d5bcf5cdb581471241c38c09e6
Nuar-Games <nuargita96@gmail.com>
2026-09-16 21:57:08 +0800

    fix: use explicit quality ternaries in 3D scene

--- src/game/arena3d/Arena3DScene.tsx
+++ src/game/arena3d/Arena3DScene.tsx
@@ -25,8 +25,8 @@ export function Arena3DScene({state,onPrimary,onSecondary,inspectOpen,transition
     <PerspectiveCamera makeDefault position={[0,6.8,9.3]} fov={46}/>
     <Arena3DCameraRig state={state} inspectOpen={inspectOpen} impactToken={impactToken}/>
 
-    <ambientLight intensity={low?.34:.24}/>
-    <hemisphereLight intensity={low?.42:.62} color="#a9ddff" groundColor="#08030b"/>
+    <ambientLight intensity={low ? .34 : .24}/>
+    <hemisphereLight intensity={low ? .42 : .62} color="#a9ddff" groundColor="#08030b"/>
     <directionalLight castShadow={!low} position={[-3.5,9,5]} intensity={1.65} color="#dcefff" shadow-mapSize-width={low?256:1024} shadow-mapSize-height={low?256:1024}/>
     <spotLight castShadow={!low} position={[0,9,1]} angle={.58} penumbra={.72} intensity={quality==='high'?42:26} distance={19} color="#f2d374"/>
     <pointLight position={[-5.8,1.7,0]} intensity={low?7:13} distance={10} color="#1aaeff"/>
@@ -46,7 +46,7 @@ function ArenaBackdrop({quality}:{quality:Arena3DQualityName}){
   return <group position={[0,0,-5.25]}>
     <mesh position={[0,2.65,0]}><boxGeometry args={[13.5,5.6,.45]}/><meshStandardMaterial color="#03070d" metalness={.66} roughness={.38}/></mesh>
     <mesh position={[0,2.6,.25]}><boxGeometry args={[7.6,.055,.055]}/><meshBasicMaterial color="#d5b552" transparent opacity={.48}/></mesh>
-    {[-5.1,-3.4,-1.7,1.7,3.4,5.1].map((x,index)=><mesh key={x} position={[x,2.5,.28]} rotation={[0,0,index<3?.18:-.18]}><boxGeometry args={[.09,4.35,.08]}/><meshBasicMaterial color={index<3?'#157fbd':'#af1f3b'} transparent opacity={low?.28:.48}/></mesh>)}
+    {[-5.1,-3.4,-1.7,1.7,3.4,5.1].map((x,index)=><mesh key={x} position={[x,2.5,.28]} rotation={[0,0,index<3 ? .18 : -.18]}><boxGeometry args={[.09,4.35,.08]}/><meshBasicMaterial color={index<3?'#157fbd':'#af1f3b'} transparent opacity={low ? .28 : .48}/></mesh>)}
     {!low&&<>
       <pointLight position={[-4.8,2.5,1]} intensity={6} distance={5} color="#168ed0"/>
       <pointLight position={[4.8,2.5,1]} intensity={6} distance={5} color="#c52b47"/>
